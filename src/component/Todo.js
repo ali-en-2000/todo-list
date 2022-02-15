59de17f52a749ebead5551bdeafcc6449fdfe3b9
ali-en-2000 <a.rezamohmmadi369@gmail.com>
2022-02-16 00:39:31 +0330

    add filter property

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -3,28 +3,86 @@ import Clock from '../img/icons8-clock-48.png'
 import '../style/Todo.css'
 import Moment from 'moment';
 import { useNavigate} from 'react-router-dom';
+import { useState } from 'react';
+const Todo = ({user,status}) => {
+    let users= JSON.parse( localStorage.getItem('user') || "[]" );
+    console.log("hi")
 
-const Todo = ({user}) => {
+    const Filter=()=>{
+        console.log(users.length)
+
+            if(status.status==='All'){
+                return (
+                    <div className='show'>
+                        <div 
+                            onClick={() => sendDetails(user)}
+                            className={user.Date===''?'todoItem-orange':'todoItem-red'} 
+                            >
+                            <div className='title' >{user.Title }
+                            <div className={user.Title===''?'hidden':'show'}>
+                            <img src={Clock} alt='' className={user.Date===''?'heidden-Clock-logo':'Clock-logo'}></img>
+                            </div>
+                            </div>
+                            <div className='decription'>{user.Description}</div>
+                            <p className={user.Description.length>65?'display':'undisplay'}>...</p>
+                            <div className='date'><p>{Moment(user.Date_create).format('DD MMM YYYY')}</p></div>
+                        </div>            
+                    </div>
+                )
+            }
+
+            if(status.status==='ByTime'){                
+                return (
+                    <div className={user.Date===''?'show':'hidden'}>
+                        <div 
+                            onClick={() => sendDetails(user)}
+                            className={user.Date===''?'todoItem-orange':'todoItem-red'} 
+                            >
+                            <div className='title' >{user.Title }
+                            <div className={user.Title===''?'hidden':'show'}>
+                            <img src={Clock} alt='' className={user.Date===''?'heidden-Clock-logo':'Clock-logo'}></img>
+                            </div>
+                            </div>
+                            <div className='decription'>{user.Description}</div>
+                            <p className={user.Description.length>65?'display':'undisplay'}>...</p>
+                            <div className='date'><p>{Moment(user.Date_create).format('DD MMM YYYY')}</p></div>
+                        </div>
+                                
+                    </div>
+                )
+                
+            }
+            if(status.status==='Deadline'){
+                return (
+                    <div className={user.Date===''?'hidden':'show'}>
+                        <div 
+                            onClick={() => sendDetails(user)}
+                            className={user.Date===''?'todoItem-orange':'todoItem-red'} 
+                            >
+                            <div className='title' >{user.Title }
+                            <div className={user.Title===''?'hidden':'show'}>
+                            <img src={Clock} alt='' className={user.Date===''?'heidden-Clock-logo':'Clock-logo'}></img>
+                            </div>
+                            </div>
+                            <div className='decription'>{user.Description}</div>
+                            <p className={user.Description.length>65?'display':'undisplay'}>...</p>
+                            <div className='date'><p>{Moment(user.Date_create).format('DD MMM YYYY')}</p></div>
+                        </div>
+                    </div>
+                
+                )
+            }
+        }    
 
     const navigate=useNavigate();
+    
     function sendDetails(user){
-        navigate('/dtails',{state:{user:user,name:'sabaoon'}});  
-        console.log('do it')          
+        navigate('/dtails',{state:{user:user}});  
     }
         return ( 
-            <>
-                <div 
-                onClick={() => sendDetails(user)}
-                className={user.Description.length>65?'todoItem-orange':'todoItem-red'} >
-                    <div className='title' >{user.Title }
-                    <img src={Clock} alt='' className={user.Description.length>65?'heidden-Clock-logo':'Clock-logo'}></img>
-                    </div>
-                    <div className='decription'>{user.Description}</div>
-                    <p className={user.Description.length>65?'display':'undisplay'}>...</p>
-                    <div className='date'><p>{Moment(user.Date).format('DD MMM YYYY')}</p></div>
-
-                </div>
-            </>
+            <div >
+                {Filter()}
+            </div>
         );
 }
  
--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import"../style/TodoList.css"
-import { useState } from 'react';
-import { useEffect } from 'react';
 ////////////////////import component
 import Todo from "./Todo"
 
-const TodoList = () => {
+const TodoList = (status) => {
 
   let users = JSON.parse( localStorage.getItem('user') || "[]" );
 
   return (
     <div className='Todolist'>
-      {users.map((users)=>(<Todo key={Math.random()} user={users}/>))}
+      {users.map((users)=>(<Todo key={Math.random()} user={users } status={status}/>))}
     </div>
   );
 
